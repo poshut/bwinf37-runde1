58fd249e04ddfac7c9bc878e79a87170ed44c27f
Richard Wohlbold <rigewo02@gmail.com>
2018-09-07 13:42:19 +0200

    5 - Move widerstand.py to Implementation

--- a5-Widerstand/Implementierung/widerstand.py
+++ a5-Widerstand/Implementierung/widerstand.py
@@ -2,16 +2,8 @@ import sys
 from itertools import combinations
 from uuid import uuid4
 
-"""
-Ein zusammengebauter Widerstand wird als Tupel repräsentiert.
-Das erste Feld ist der Typ: "einer", "reihe" oder "parallel"
-Falls der Widerstandstyp "einer" ist, so ist das zweite Feld die Ohmzahl.
-Falls der Widerstandstyp "reihe" ist, so sind das zweite und dritte Feld die in Reihe geschaltenen Widerstände.
-Falls der Widerstandstyp "parallel" ist, so sind das zweite und dritte Feld die parallel geschaltenen Widerstände.
-"""
 
-
-def berechne(widerstand):
+def widerstandswert_berechnen(widerstand):
     """
     Den Widerstandswert eines Widerstandes zurueckgeben
     @param widerstand: Der Widerstand, fuer den der Widerstandswert berechnet werden soll
@@ -19,9 +11,9 @@ def berechne(widerstand):
     if widerstand[0] == "einer":
         return widerstand[1]
     elif widerstand[0] == "reihe":
-        return berechne(widerstand[1]) + berechne(widerstand[2])
+        return widerstandswert_berechnen(widerstand[1]) + widerstandswert_berechnen(widerstand[2])
     elif widerstand[0] == "parallel":
-        return 1 / (1 / berechne(widerstand[1]) + 1 / berechne(widerstand[2]))
+        return 1 / (1 / widerstandswert_berechnen(widerstand[1]) + 1 / widerstandswert_berechnen(widerstand[2]))
     print("error")
     exit(1)
 
@@ -33,6 +25,8 @@ def widerstand_zusammenbauen(ohm, wie_viele, widerstaende):
     @param wie_viele: Aus wie vielen Elementarwiderstaenden der Widerstand bestehen soll
     @param widerstaende: Liste von zu verbauenden Elementarwiderstaenden
     """
+    assert 1 <= wie_viele <= 4
+
     if wie_viele == 1:
         return ("einer", min(widerstaende, key=lambda x: abs(x-ohm)))
 
@@ -45,45 +39,55 @@ def widerstand_zusammenbauen(ohm, wie_viele, widerstaende):
             widerstaende.remove(combination[0])
             widerstaende.remove(combination[1])
 
-            # Fall 1: Die Gruppen werden in Reihe geschalten
+            # Fall 1: Die Gruppen werden in Reihe geschalten, R1 ist aber parallel geschalten
             uebrig = ohm - 1 / (1 / combination[0] + 1 / combination[1])
             moeglichkeiten.append(("reihe", ("parallel", ("einer", combination[0]), (
                 "einer", combination[1])), widerstand_zusammenbauen(uebrig, 2, widerstaende)))
 
-            # Fall 2: Die Gruppen werden parallel geschalten
-            uebrig = 1 / (1 / ohm - (1 / combination[0] + 1 / combination[1]))
-            moeglichkeiten.append(("parallel", ("parallel", ("einer", combination[0]), (
+            # Fall 2: Die Gruppen werden parallel geschalten, R1 ist aber in Reihe geschalten
+            try:
+                uebrig = 1 / (1 / ohm - 1 / (combination[0] + combination[1]))
+            except ZeroDivisionError:
+                # Wenn in der ersten Klammer 0 rauskommt, tritt ein Fehler auf.
+                # In diesem Fall ist die Ohmzahl durch die beiden Widerstände gedeckt und die anderen
+                # Widerstände sollten so hoch wie möglich sein
+                uebrig = 1e100
+
+            moeglichkeiten.append(("parallel", ("reihe", ("einer", combination[0]), (
                 "einer", combination[1])), widerstand_zusammenbauen(uebrig, 2, widerstaende)))
+            
 
             widerstaende.append(combination[0])
             widerstaende.append(combination[1])
 
     # Fuer jeden Widerstand zweri Moeglichkeiten erstellen: In Reihe und parallel
-    for i in widerstaende.copy():
-        widerstaende.remove(i)
+    for r1 in set(widerstaende):
+        widerstaende.remove(r1)
 
         # Man kann den neuen Widerstand und den Rest in Reihe schalten
-        rest_reihe = ohm - i
-        moeglichkeiten.append(("reihe", ("einer", i), widerstand_zusammenbauen(
+        rest_reihe = ohm - r1
+        moeglichkeiten.append(("reihe", ("einer", r1), widerstand_zusammenbauen(
             (rest_reihe), wie_viele-1, widerstaende)))
 
         # Mann kann den neuen Widerstand und den Rest parallel schalten
-
-        # Wenn Ohm = i, oder Ohm = 0, dann kann es zu Fehlern fuehren. In diesem Fall betrachten wir den Rest als sehr gross
-        if ohm != i and ohm != 0:
-            rest_parallel = 1 / ((1 / ohm) - (1 / i))
-        else:
+        try:
+            rest_parallel = 1 / ((1 / ohm) - (1 / r1))
+        except ZeroDivisionError:
+            # Wenn in der ersten Klammer 0 rauskommt, tritt ein Fehler auf.
+            # In diesem Fall ist die Ohmzahl durch r1 gedeckt und die anderen
+            # Widerstände sollten so hoch wie möglich sein
             rest_parallel = 1e100
-        moeglichkeiten.append(("parallel", ("einer", i), widerstand_zusammenbauen(
+
+        moeglichkeiten.append(("parallel", ("einer", r1), widerstand_zusammenbauen(
             (rest_parallel), wie_viele-1, widerstaende)))
 
-        widerstaende.append(i)
+        widerstaende.append(r1)
 
     # Die Moeglichkeit mit der hoechsten Genauigkeit finden
-    return min(moeglichkeiten, key=lambda x: abs(berechne(x)-ohm))
+    return min(moeglichkeiten, key=lambda x: abs(widerstandswert_berechnen(x)-ohm))
 
 
-def draw(widerstand, graph, knoten_davor, knoten_danach):
+def widerstand_zeichnen(widerstand, graph, knoten_davor, knoten_danach):
     """
     Den Graphen mit der Bibliothek "graphviz" zeichnen
     Jeder Knoten bekommt eine zufällige ID.
@@ -98,11 +102,20 @@ def draw(widerstand, graph, knoten_davor, knoten_danach):
     elif widerstand[0] == "reihe":
         uid = str(uuid4())
         graph.node(uid, label='', height='0', shape='none')
-        draw(widerstand[1], graph, knoten_davor, uid)
-        draw(widerstand[2], graph, uid, knoten_danach)
+        widerstand_zeichnen(widerstand[1], graph, knoten_davor, uid)
+        widerstand_zeichnen(widerstand[2], graph, uid, knoten_danach)
     elif widerstand[0] == "parallel":
-        draw(widerstand[1], graph, knoten_davor, knoten_danach)
-        draw(widerstand[2], graph, knoten_davor, knoten_danach)
+        widerstand_zeichnen(widerstand[1], graph, knoten_davor, knoten_danach)
+        widerstand_zeichnen(widerstand[2], graph, knoten_davor, knoten_danach)
+
+def eingabe_lesen(dateiname):
+    widerstaende = []
+    with open(sys.argv[1]) as f:
+        for i in f.read().split('\n'):
+            if i != '':
+                assert i != "0"
+                widerstaende.append(int(i))
+    return widerstaende
 
 
 if __name__ == '__main__':
@@ -111,9 +124,8 @@ if __name__ == '__main__':
               "<Widerstandsliste> <Widerstandswert>")
         exit(1)
 
+    widerstaende = eingabe_lesen(sys.argv[1])
     widerstandswert = int(sys.argv[2])
-    with open(sys.argv[1]) as f:
-        widerstaende = list(map(lambda x: float(int(x[:-1])), f.readlines()))
 
     # Versuche, graphviz zu importieren
     try:
@@ -126,17 +138,17 @@ if __name__ == '__main__':
     for anzahl in range(1, 5):
         resultat = widerstand_zusammenbauen(
             widerstandswert, anzahl, widerstaende)
-        print(anzahl, "Widerstände:", berechne(resultat), "Ohm")
+        print(anzahl, "Widerstände:", widerstandswert_berechnen(resultat), "Ohm")
         print(resultat)
 
         if graphviz is not None:
-            name = "%s Widerstände - %.2f Ohm" % (anzahl, berechne(resultat))
+            name = "%s Widerstände - %.2f Ohm" % (anzahl, widerstandswert_berechnen(resultat))
             graph = graphviz.Graph(name=name, node_attr={
                                    'shape': 'box'}, format='png')
 
             graph.node('-', label='-')
             graph.node('+', label='+')
-            draw(resultat, graph, '-', '+')
+            widerstand_zeichnen(resultat, graph, '-', '+')
             graph.render()
             print("Zeichnung nach \"%s.gv.png\" ausgegeben" % name)
 
